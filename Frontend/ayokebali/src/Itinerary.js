d22daf5c1524999442a07e3f2550ff2497c7c32a
Rei Arriel Clyfton <92192967+RClyf@users.noreply.github.com>
2023-12-02 23:08:10 +0700

    feat: update on itinerary.js

--- Frontend/ayokebali/src/Itinerary.js
+++ Frontend/ayokebali/src/Itinerary.js
@@ -16,23 +16,48 @@ const Itinerary = () => {
   const username = sessionStorage.getItem('username'); // Ambil username dari session
 
   useEffect(() => {
-    const storedToken1 = sessionStorage.getItem('token1');
-    setToken1(storedToken1 || ''); // Jika tidak ada token, gunakan string kosong
+    const fetchData = async () => {
+      const storedToken1 = sessionStorage.getItem('token1');
+      setToken1(storedToken1 || ''); // Jika tidak ada token, gunakan string kosong
 
-    if (username) {
-      // Fetch data itinerary berdasarkan username dari API
-      axios.get(`https://ayokebalitst.azurewebsites.net/itinerary/user/${username}`, {
-        headers: {
-          Authorization: `Bearer ${storedToken1}` // Menyertakan token dalam header Authorization
-        }
-      })
-        .then(response => {
-          setItineraries(response.data);
-        })
-        .catch(error => {
+      if (username) {
+        try {
+          // Fetch data itinerary berdasarkan username dari API
+          const response = await axios.get(`https://ayokebalitst.azurewebsites.net/itinerary/user/${username}`, {
+            headers: {
+              Authorization: `Bearer ${storedToken1}` // Menyertakan token dalam header Authorization
+            }
+          });
+
+          // Ambil data detail destinasi untuk setiap destination_id dalam itinerary
+          const itinerariesWithDestinations = await Promise.all(
+            response.data.map(async (itinerary) => {
+              const destinationDetails = await Promise.all(
+                itinerary.destination.map(async (destinationId) => {
+                  const destinationResponse = await axios.get(`https://ayokebalitst.azurewebsites.net/destination/${destinationId}`, {
+                    headers: {
+                      Authorization: `Bearer ${storedToken1}` // Menyertakan token dalam header Authorization
+                    }
+                  });
+                  return destinationResponse.data;
+                })
+              );
+
+              return {
+                ...itinerary,
+                destination: destinationDetails,
+              };
+            })
+          );
+
+          setItineraries(itinerariesWithDestinations);
+        } catch (error) {
           console.error('Error fetching itinerary data:', error);
-        });
-    }
+        }
+      }
+    };
+
+    fetchData();
   }, [username]);
 
   return (
@@ -42,15 +67,15 @@ const Itinerary = () => {
       {itineraries.map(itinerary => (
         <Box key={itinerary.id} borderWidth="1px" borderRadius="lg" p={4} mb={4}>
           <VStack align="start">
-            <Text>Username: {itinerary.username}</Text>
+            <Text>Tanggal: {itinerary.date}</Text>
             <Text>Lama Kunjungan: {itinerary.lama_kunjungan} hari</Text>
             <Text>Accommodation: {itinerary.accommodation}</Text>
 
             <HStack>
               <Text>Destinations:</Text>
-              {itinerary.destination.map(destinationId => (
-                <Badge key={destinationId} colorScheme="blue" ml={1}>
-                  {destinationId}
+              {itinerary.destination.map(destination => (
+                <Badge key={destination.destination_id} colorScheme="blue" ml={1}>
+                  {destination.name} ({destination.location})
                 </Badge>
               ))}
             </HStack>
